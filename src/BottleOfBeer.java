6226498429eb19d9c6f9eaf3047704e84305261b
Martin Langenbacher <martin.langenbacher@telekom.de>
2021-09-13 22:10:30 +0200

    99 bottles

---
+++ src/BottleOfBeer.java
@@ -0,0 +1,28 @@
+public class BottleOfBeer {
+
+    public static void main(String[] args) {
+
+        System.out.println("Lyrics of the song 99 Bottles of Beer");
+
+        for(int i=99; i > 1; i--) {
+            System.out.println();
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            int b = i-1;
+
+            if ( i > 2 ) {
+                System.out.println("Take one down and pass it around, " + b + " bottles of beer on the wall.");
+            } else {
+                System.out.println("Take one down and pass it around, " + b + " bottle of beer on the wall.");
+            }
+        }
+
+        System.out.println();
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down and pass it around, no more bottles of beer on the wall.");
+        System.out.println();
+        System.out.println("No more bottles of beer on the wall, no more bottles of beer.");
+        System.out.println("Go to the store and buy some more, 99 bottles of beer on the wall.");
+
+    }
+
+}
