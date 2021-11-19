9696231bc168db227072c52d54b0031822541334
Martin Langenbacher <martin.langenbacher@telekom.de>
2021-11-19 11:47:11 +0100

    Mit der inneren Classe

--- src/main/java/session5/Car.java
+++ src/main/java/session5/Car.java
@@ -63,8 +63,48 @@ public class Car {
 
     // is a factory method
     public static CarBuilder newBuilder () {
-
         return new CarBuilder();
+    }
+
+
+// inner Class...
+// Warum muss hier static stehen ????????  <===================
+    public static class CarBuilder {
+
+        String brand;
+        String color;
+        int price;
+        String model;
+
+
+        public CarBuilder brand(String brandName) {
+            this.brand = brandName;
+            return this;
+        }
+
+        public CarBuilder color(String colorName) {
+            this.color = colorName;
+            return this;
+        }
+
+        public CarBuilder price(int priceAmount) {
+            this.price = priceAmount;
+            return this;
+        }
+
+        public CarBuilder model(String modelName) {
+            this.model = modelName;
+            return this;
+        }
+
+        public Car build() {
+            // Achtung: Hier muss die Reihenfolge stimmen --> Siehe Car-Konstruktor
+            Car car = new Car(brand, model, price, color);
+            return car;
+        }
+
 
     }
+
+
 }
